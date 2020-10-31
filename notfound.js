986ea797aca88790c52844433aaf2b0735f2cbf1
daspharion <daspharion@gmail.com>
2020-10-31 16:33:35 +0100

    feat: brotli compression

--- notfound.js
+++ notfound.js
@@ -1,13 +1,15 @@
 const { readFileSync } = require('fs')
 const { createServer } = require('http')
+const { brotliCompressSync } = require('zlib')
 
 const { PORT } = process.env
 
-const index = readFileSync('index.html')
+const index = brotliCompressSync(readFileSync('index.html'))
 
 createServer()
   .on('request', (req, res) => {
     res.setHeader('Content-Type', 'text/html')
+    res.setHeader('Content-Encoding', 'br')
     res.writeHead(200)
     res.end(index)
   })
